c69f0d9b55bc2677a160e3b2c4e52d9b6780285a
nwilliams48 <77929371+nwilliams48@users.noreply.github.com>
2021-03-26 06:57:34 -0400

    Imported index, css, and js code

---
+++ src/index.js
@@ -0,0 +1,68 @@
+function currentDate(now) {
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+  let day = days[now.getDay()];
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = now.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+
+  return `${day} ${hour}:${minute}`;
+}
+
+function currentTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${temperature}°F`;
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${city.value}`;
+
+  let apiKey = "33098ebc3e6d70b785afa638f3f87592";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=imperial`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(currentTemperature);
+}
+
+function showCurrentTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let heading = document.querySelector("h2");
+  heading.innerHTML = `${temperature}℉`;
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${response.data.name}`;
+}
+
+function retrievePosition(position) {
+  console.log(position);
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "33098ebc3e6d70b785afa638f3f87592";
+  let units = "imperial";
+  let apiLatUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiLatUrl).then(showCurrentTemperature);
+}
+
+function getPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+
+let currentButton = document.querySelector("#current-location");
+currentButton.addEventListener("click", getPosition);
+
+let now = new Date();
+
+let currentDay = document.querySelector("#current-day");
+currentDay.innerHTML = currentDate(now);
+
+let form = document.querySelector("#search-form");
+
+form.addEventListener("submit", search);
